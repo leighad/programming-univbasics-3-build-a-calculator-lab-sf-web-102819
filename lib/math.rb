fa500ba2a4faf13984414c9d21e77c234b477093
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-10-04 15:25:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def division(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def square_root(num)
-
+  sum = num1 + num2
+  puts sum
 end
